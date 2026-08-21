d3447e9187240effc5d7b05b9ff6b82aac5f2c09
earthinghann-boop <hann33885@qq.com>
2026-08-21 16:54:17 +0800

    cleanup

--- check2buggy.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var h = fs.readFileSync('index.html', 'utf8');
-var re = /goProductSlide_\w+\([\s\S]*?\)/g;
-var m;
-while ((m = re.exec(h)) !== null) {
-    var s = m[0];
-    if (s.match(/,/)) console.log(s.slice(0, 100));
-}
