6ff9c320e19809b8432a6e8bdc87a5e39301b637
earthinghann-boop <hann33885@qq.com>
2026-08-21 16:45:40 +0800

    cleanup: remove 18 diagnostic scripts

--- reset_index.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-var { execSync } = require('child_process');
-execSync('git checkout HEAD -- index.html', {cwd: 'C:/Users/18574/.qclaw/workspace/earthinghealth-website'});
-var h = fs.readFileSync('index.html', 'utf8');
-console.log('Restored. Size:', h.length);
